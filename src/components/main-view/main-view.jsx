6d58379e9cf439d0da6540d1bc8993ccfbe057e6
amelieberry <aac.berry@gmail.com>
2022-10-26 16:50:02 +0200

    imported MovieView into MainView component

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
 
 // Create mainView using React.Component and expose it
 export class MainView extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            movies: [
+                {_id: 1, Title: 'Inception', Description: 'description 1', ImagePath: '...'},
+                {_id: 2, Title: 'The Shawshank Redemption', Description: 'description 2', ImagePath: '...'},
+                {_id: 3, Title: 'Gladiator', Description: 'description 3', ImagePath: '...'}
+            ],
+            selectedMovie: null
+        };
+    }
     render() {
-        const { movies } = this.state;
+        const { movies, selectedMovie } = this.state;
+
+        if (selectedMovie) return <MovieView movie={selectedMovie}/>
 
         if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
         
         return (
             <div className="main-view">
+                <button onClick={() => {alert('Nice!')}}>Click me!</button>
                 {movies.map(movie => <MovieCard  key={movie._id} movieData={movie}/>)}
             </div>
         );
--- src/index.jsx
+++ src/index.jsx
@@ -6,16 +6,6 @@ import './index.scss';
 
 // main component
 class TrackmApplication extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            movies: [
-                {_id: 1, Title: 'Inception', Description: 'description 1', ImagePath: '...'},
-                {_id: 2, Title: 'The Shawshank Redemption', Description: 'description 2', ImagePath: '...'},
-                {_id: 3, Title: 'Gladiator', Description: 'description 3', ImagePath: '...'}
-            ]
-        }
-    }
     render() {
         return (
             <MainView />
