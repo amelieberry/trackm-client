765a2c5b02a8ca1de9699dcf30da05427da5538b
amelieberry <aac.berry@gmail.com>
2022-10-26 13:16:47 +0200

    replaced the UI returned by TrackmApplication

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MainView } from './components/main-view/main-view';
 
 import './index.scss';
 
@@ -7,9 +8,7 @@ import './index.scss';
 class TrackmApplication extends React.Component {
     render() {
         return (
-            <div className ="trackm">
-                <div>Good morning</div>
-            </div>
+            <MainView />
         );
     }
 }
