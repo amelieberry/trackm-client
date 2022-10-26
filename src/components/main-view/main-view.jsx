9206614464aa8fadc619da9a90865f9694b617c2
amelieberry <aac.berry@gmail.com>
2022-10-26 14:30:37 +0200

    imported MovieCard into MainView component

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { MovieCard } from '../movie-card/movie-card';
 
 // Create mainView using React.Component and expose it
 export class MainView extends React.Component {
     render() {
+        const { movies } = this.state;
+
+        if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+        
         return (
             <div className="main-view">
-                <div>Inception</div>
-                <div>The Shawshank Redemption</div>
-                <div>Gladiator</div>
+                {movies.map(movie => <MovieCard  key={movie._id} movieData={movie}/>)}
             </div>
         );
     }
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 export class MovieCard extends React.Component {
     render() {
-        return <div className="movie-card">A Title</div>;
+        const { movieData } = this.props;
+        return <div className="movie-card">{movieData.Title}</div>;
     }
 }
