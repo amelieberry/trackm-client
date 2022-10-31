3780e57c953886948a828efd56ed4ed50f860043
amelieberry <aac.berry@gmail.com>
2022-10-31 14:34:39 +0100

    updated forms to react-bootstrap Forms

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Form } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 export function LoginView(props) {
     const [ username, setUsername ] = useState('');
@@ -13,21 +15,18 @@ export function LoginView(props) {
     };
 
     return (
-        <form>
-            <label>
-                Are you a new user?  
-                <button>Register</button><br />
-            </label>
-            <label>
-                Username: 
-                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-            </label>
-            <label>
-                Password: 
-                <input type="text" value={password} onChange={e => setPassword(e.target.value)} />
-            </label>
-            <button type="submit" onClick={handleSubmit}>Login</button>
-        </form>
+        <Form>
+            <Form.Group>
+                <Form.Label>Username: </Form.Label>
+                <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} />
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>Password: </Form.Label>
+                <Form.Control type="text" value={password} onChange={e => setPassword(e.target.value)} />
+            </Form.Group>
+            <Button variant="primary" type="submit" onClick={handleSubmit}>Login</Button>
+        </Form>
     )
 }
 
--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 export function RegistrationView(props) {
     const [ username, setUsername ] = useState('');
@@ -15,25 +17,29 @@ export function RegistrationView(props) {
     };
 
     return (
-        <form>
-            <label>
-                Username: 
-                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-            </label>
-            <label>
-                Password: 
-                <input type="text" value={password} onChange={e => setPassword(e.target.value)} />
-            </label>
-            <label>
-                Email: 
-                <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
-            </label>
-            <label>
-                Birthday: 
-                <input type="date" value={birthday} onChange={e => setBirthday(e.target.value)} />
-            </label>
-            <button type="submit" onClick={handleSubmit}>Submit</button>
-        </form>
+        <Form>
+            <Form.Group>
+                <Form.Label>Username: </Form.Label>
+                <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} />
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>Password: </Form.Label>
+                <Form.Control type="text" value={password} onChange={e => setPassword(e.target.value)} />
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>Email: </Form.Label>
+                <Form.Control type="email" value={email} onChange={e => setEmail(e.target.value)} />
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>Birthday: </Form.Label>
+                <Form.Control type="date" value={birthday} onChange={e => setBirthday(e.target.value)} />
+            </Form.Group>
+
+            <Button variant="primary" type="submit" onClick={handleSubmit}>Register</Button>
+        </Form>
     )
 }
 
