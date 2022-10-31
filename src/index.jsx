d2a9c13dc7406a402734f1bbbd5efb59d9136955
amelieberry <aac.berry@gmail.com>
2022-10-31 14:39:57 +0100

    added Container Bootstrap Component

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Container } from 'react-bootstrap/Container';
 import { MainView } from './components/main-view/main-view';
 
 import './index.scss';
@@ -8,7 +9,9 @@ import './index.scss';
 class TrackmApplication extends React.Component {
     render() {
         return (
-            <MainView />
+            <Container>
+                <MainView />
+            </Container>
         );
     }
 }
