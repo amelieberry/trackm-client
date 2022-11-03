a209eebfa0c2fc271e8b76bcce10c7e5646916db
Amelie <aac.berry@gmail.com>
2022-11-03 13:20:57 +0100

    LoginView now posts to API for authentication

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -3,16 +3,25 @@ import PropTypes from 'prop-types';
 import { Form, Button, Card, CardGroup, Container, Col, Row } from 'react-bootstrap';
 
 import './login-view.scss';
+import axios from 'axios';
 
 export function LoginView(props) {
-    const [ username, setUsername ] = useState('');
-    const [ password, setPassword ] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(username, password);
-        // send request to server for authentication then call props.onLoggedIn(username)
-        props.onLoggedIn(username);
+
+        try {
+            // send request to server for authentication then call props.onLoggedIn(response.data)
+            const response = await axios.post('https://trackm-app.herokuapp.com/login', {
+                Username: username,
+                Password: password
+            })
+            props.onLoggedIn(response.data);
+        } catch (error) {
+            console.log(error, 'no such user');
+        }
     };
 
     return (
@@ -28,10 +37,10 @@ export function LoginView(props) {
                                 <Form>
                                     <Form.Group>
                                         <Form.Label>Username: </Form.Label>
-                                        <Form.Control 
-                                            type="text" 
-                                            value={username} 
-                                            onChange={e => setUsername(e.target.value)} 
+                                        <Form.Control
+                                            type="text"
+                                            value={username}
+                                            onChange={e => setUsername(e.target.value)}
                                             placeholder="Enter your username"
                                             required
                                         />
@@ -39,10 +48,10 @@ export function LoginView(props) {
 
                                     <Form.Group>
                                         <Form.Label>Password: </Form.Label>
-                                        <Form.Control 
-                                            type="password" 
-                                            value={password} 
-                                            onChange={e => setPassword(e.target.value)} 
+                                        <Form.Control
+                                            type="password"
+                                            value={password}
+                                            onChange={e => setPassword(e.target.value)}
                                             placeholder="Enter your password"
                                             required
                                         />
