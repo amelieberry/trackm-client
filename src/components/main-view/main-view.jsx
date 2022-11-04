588d15d16f9f6c9aca4af22eb95e80f5cad98168
Amelie <aac.berry@gmail.com>
2022-11-04 18:05:57 +0100

    created files for profile, director and genre view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -10,6 +10,9 @@ import { RegistrationView } from '../registration-view/registration-view';
 import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { DirectorView } from '../director-view/director-view';
+import { GenreView } from '../genre-view/genre-view';
+import { ProfileView } from '../profile-view/profile-view';
 
 import './main-view.scss';
 
