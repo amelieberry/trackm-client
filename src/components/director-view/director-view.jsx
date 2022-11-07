a81884b718bcc4bf68d1a9448f8d99fd4438de51
Amelie <aac.berry@gmail.com>
2022-11-07 18:57:28 +0100

    wip: added content to DirectorView

---
+++ src/components/director-view/director-view.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Row, Col, Button, Container } from 'react-bootstrap';
+
+import { useNavigate } from 'react-router-dom';
+
+import './director-view.scss';
+
+export function DirectorView({director}) {
+
+    const navigate = useNavigate();
+
+    return (
+        <Container xl={8} className="director-view">
+            <h1>Hello</h1>
+            <Row className="director-name text-center" lg={10}>
+                <Col>
+                    <h2 className="value">{director.Name}</h2>
+                </Col>
+            </Row>
+            <Row className="movie-details">
+                <Col>
+                    <div className="director-details">
+                        <h4 className="label">Biography: </h4>
+                        <span className="value">{director.Bio}</span>
+                    </div>
+                    <div className="director-details">
+                        <h4 className="label">Born in: </h4>
+                        <span className="value">{director.Birth}</span>
+                    </div>
+                    <div className="director-details"> 
+                        <h4 className="label">Died in: </h4>
+                        <span className="value">{director.Death}</span>
+                    </div>
+                    <div className="director-details">
+                        <h4 className="label">Movies directed by {director.Name}: </h4>
+                    </div>
+                    <Button className='movie-details' variant="secondary" onClick={() => navigate(-1)}>Back</Button>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
