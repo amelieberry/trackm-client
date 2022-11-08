d303b3858003919e2a684d9c1b92326753d7198b
Amelie <aac.berry@gmail.com>
2022-11-08 23:27:45 +0100

    added content to FavoriteMovie component

---
+++ src/components/profile-view/favorite-movies.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import { Button, Card, Col, Row } from 'react-bootstrap';
+
+import { Link } from 'react-router-dom';
+
+export function FavoriteMovies({ favoriteMoviesList, removeFav }) {
+    return (
+        <div>
+            <Row>
+                <h2>Favorite Movies</h2>
+            </Row>
+            <Row>
+                <Col className="card-columns">
+                    {favoriteMoviesList.map(movies => (
+                        <Card key={movies._id} bg="dark" style={{ margin: '10px' }}>
+                            <Card.Img crossOrigin="anonymous" variant="top" src={movies.ImagePath} />
+                            <Card.Body>
+                                <Link to={`/movies/${movies._id}`}>
+                                    <Card.Title>{movies.Title}</Card.Title>
+                                </Link>
+                                <Row className="d-flex justify-content-end align-items-center">
+                                    <Button variant="secondary" onClick={() => removeFav(movies._id)}>Unfavorite</Button>
+                                </Row>
+                            </Card.Body>
+                        </Card>
+                    ))}
+                </Col>
+            </Row>
+        </div>     
+    )
+}
