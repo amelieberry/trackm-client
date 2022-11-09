6e04a50d56222f8d2f31b2d53888ac248dae3f2c
Amelie <aac.berry@gmail.com>
2022-11-09 18:33:27 +0100

    fixed user update, now logs out on username change

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import { useNavigate } from 'react-router-dom';
+
 import { Container, Col, Row, CardGroup, Card, Button } from 'react-bootstrap';
 
 import { FavoriteMovies } from './favorite-movies';
@@ -14,6 +16,9 @@ export function ProfileView({ movies }) {
     const [user, setUser] = useState();
     const token = localStorage.getItem("token");
     const currentUser = localStorage.getItem("user");
+
+    const navigate = useNavigate();
+
 
     const getUser = async () => {
         try {
@@ -43,15 +48,22 @@ export function ProfileView({ movies }) {
     const handleSubmit = async (e, username, newPassword, email, user) => {
         e.preventDefault();
         try {
-            const updateObject = {
+            let updateObject = {
                 Username: username ? username : user.Username,
-                Password: newPassword ? newPassword : user.Password,
-                Email: email ? email : user.Email
+                Email: email ? email : user.Email,
             };
+            if (newPassword) {
+                updateObject.Password = newPassword
+            }
             const response = await axios.put(`https://trackm-app.herokuapp.com/users/${user.Username}`, updateObject, {
                 headers: { Authorization: `Bearer ${token}` },
             });
-            console.log(response.data);
+            console.log(updateObject);
+            if (username) {
+                alert('You will now be logged out, please log back in with the updated username');
+                localStorage.clear();
+                window.open("/", "_self")
+            }
         } catch (error) {
             console.log(error)
         }
@@ -62,10 +74,9 @@ export function ProfileView({ movies }) {
             const response = await axios.delete(`https://trackm-app.herokuapp.com/users/${currentUser}`, {
                 headers: { Authorization: `Bearer ${token}` }
             });
-            alert('Your account was permanently deleted')
+            alert('Your account was permanently deleted');
             localStorage.clear();
-            setUser(response.data);
-            window.open('/', 'self')
+            window.open('/', '_self');
         } catch (error) {
             console.log(error);
         }
@@ -76,7 +87,11 @@ export function ProfileView({ movies }) {
     }, []);
 
     useEffect(() => {
-        setFavoriteMovies(movies.filter((movie) => user.FavoriteMovies.includes(movie._id)));
+        if (!user) {
+            setFavoriteMovies([])
+        } else {
+            setFavoriteMovies(movies.filter((movie) => user.FavoriteMovies.includes(movie._id)));
+        }
     }, [user])
 
     return (
@@ -87,17 +102,13 @@ export function ProfileView({ movies }) {
                 <div className="profile-view d-flex flex-column justify-content-center align-items-center">
                     <h1 className='profile-username'>{user.Username}'s Profile</h1>
                     <Row>
-                        <Col>
-                            <p>Email: {user.Email}</p>
-                        </Col>
+                        <p>Email: {user.Email}</p>
                     </Row>
-                    <Row className="card-columns">
-                        {(!favoriteMovies) ?
-                            <div className="main-view"></div>
-                            :
-                            <FavoriteMovies favoriteMoviesList={favoriteMovies} removeFav={(movieId) => unfavorite(movieId)} />
-                        }
-                    </Row>
+                    {(!favoriteMovies) ?
+                        <div className="main-view"></div>
+                        :
+                        <FavoriteMovies favoriteMoviesList={favoriteMovies} removeFav={(movieId) => unfavorite(movieId)} />
+                    }
                     <UpdateUser handleSubmit={handleSubmit} user={user} />
                     <Row>
                         <h2 className="update-title">Delete User Info</h2>
