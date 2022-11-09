9a6a1f5dd7cbeba7d989d3ff517d951c7d5963cf
Amelie <aac.berry@gmail.com>
2022-11-09 02:40:53 +0100

    movieCard button now posts favorite movie

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -4,32 +4,47 @@ import { Card, Button, Col, Row } from 'react-bootstrap';
 
 import { Link } from "react-router-dom";
 
+import axios from 'axios';
+
 import './movie-card.scss';
 
 
 export function MovieCard({movie}) {
-        return (
-                <Card
-                    bg="dark"
-                    style={{ margin: '10px' }}
-                >
-                    <Card.Img crossOrigin="anonymous" variant="top" src={movie.ImagePath} />
-                    <Card.Body>
-                        <Card.Title>{movie.Title}</Card.Title>
-                        <Row>
-                            <Col>
-                                <Card.Text className="card-genre">{movie.Genre.Name}</Card.Text>
-                                <Card.Text className="card-release">{movie.ReleaseYear}</Card.Text>
-                            </Col>
-                            <Col className="d-flex justify-content-end align-items-center">
-                                <Link to={`/movies/${movie._id}`}>
-                                    <Button variant="info">Open</Button>
-                                </Link>
-                            </Col>
-                        </Row>
-                    </Card.Body>
-                </Card>
-        );
+    const token = localStorage.getItem("token");
+    const currentUser = localStorage.getItem("user");
+
+    const addFavorite = async (movieId) => {
+        console.log(movieId, currentUser)
+        try {
+        const response = await axios.post(`https://trackm-app.herokuapp.com/users/${currentUser}/movies/${movieId}`,{} ,{
+                headers: { Authorization: `Bearer ${token}` }
+            });
+            // setUser(response.data);
+            // setFavoriteMovies(movies.filter((movie) => response.data.FavoriteMovies.includes(movie._id)))
+        } catch (error) {
+            console.error(error, 'Could not add movie to favorites');
+        }
+    }
+    return (
+        <Card bg="dark" style={{ margin: '10px' }}>
+            <Card.Img crossOrigin="anonymous" variant="top" src={movie.ImagePath} />
+            <Button className="favorite-button" variant="secondary" onClick={() => addFavorite(movie._id)}>Add to Favorite</Button>
+            <Card.Body>
+                <Card.Title>{movie.Title}</Card.Title>
+                <Row>
+                    <Col>
+                        <Card.Text className="card-genre">{movie.Genre.Name}</Card.Text>
+                        <Card.Text className="card-release">{movie.ReleaseYear}</Card.Text>
+                    </Col>
+                    <Col className="d-flex justify-content-end align-items-center">
+                        <Link to={`/movies/${movie._id}`}>
+                            <Button variant="info">Open</Button>
+                        </Link>
+                    </Col>
+                </Row>
+            </Card.Body>
+        </Card>
+    );
 }
 
 MovieCard.propTypes = {
