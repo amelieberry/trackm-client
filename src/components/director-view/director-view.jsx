302f96013bf389bae73cb0f1838f25fdf1cd938d
Amelie <aac.berry@gmail.com>
2022-11-09 14:42:43 +0100

    implemented the director and genre views

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,26 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Row, Col, Button, Container } from 'react-bootstrap';
 
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import { MovieCard } from '../movie-card/movie-card';
 
+import axios from 'axios';
+
 import './director-view.scss';
 
-export function DirectorView({director, directorMovies}) {
+export function DirectorView() {
+    const [director, setDirector] = useState();
+
+    const params = useParams();
+    const directorName = params.name;
+    console.log(directorName);
 
     const navigate = useNavigate();
 
+    const token = localStorage.getItem("token");
+
+    const getDirector = async () => {
+        try {
+            const response = await axios.get(`https://trackm-app.herokuapp.com/movies/Director/${directorName}`, {
+                headers: { Authorization: `Bearer ${token}` }
+            });
+            setDirector({
+                ...response.data.director.Director,
+                matchingMovies: response.data.matchingMovies
+            });
+        } catch (error) {
+            console.log(error, 'could not GET User');
+        }
+    }
+
+    useEffect(() => {
+        getDirector();      
+    }, []);
+
     return (
         <Container xl={8} className="director-view">
-            <h1>Hello</h1>
-            <Row className="director-name text-center" lg={10}>
+            {(!director) ? 
+            <p>loading</p>
+            :
+            <div>
+                <Row className="director-name text-center" lg={10}>
                 <Col>
-                    <h2 className="value">{director.Director.Name}</h2>
+                    <h2 className="value">{director.Name}</h2>
                 </Col>
             </Row>
-            <Row className="movie-details">
+             <Row className="movie-details">
                 <Col>
                     <div className="director-details">
                         <h4 className="label">Biography: </h4>
@@ -38,7 +68,7 @@ export function DirectorView({director, directorMovies}) {
                         <h4 className="label">Movies directed by {director.Name}: </h4>
                         <Row>
                             <Col className="card-columns">
-                                {directorMovies.map(movie => (
+                                {director.matchingMovies.map(movie => (
                                     <MovieCard key={movie._id} movie={movie} />
                                 ))}
                             </Col>
@@ -47,6 +77,9 @@ export function DirectorView({director, directorMovies}) {
                     <Button className='movie-details' variant="secondary" onClick={() => navigate(-1)}>Back</Button>
                 </Col>
             </Row>
+            </div> 
+            }
+            
         </Container>
     );
 }
--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -1,44 +1,76 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Row, Col, Button, Container } from 'react-bootstrap';
 
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import axios from 'axios';
 
 import { MovieCard } from '../movie-card/movie-card';
 
 import './genre-view.scss';
 
-export function GenreView({genre, genreMovies}) {
+export function GenreView() {
+    const [genre, setGenre] = useState();
+
+    const params = useParams();
+    const genreName = params.name;
+    console.log(genreName);
 
     const navigate = useNavigate();
 
+    const token = localStorage.getItem("token");
+
+    const getGenre = async () => {
+        try {
+            const response = await axios.get(`https://trackm-app.herokuapp.com/movies/Genre/${genreName}`, {
+                headers: { Authorization: `Bearer ${token}` }
+            });
+            setGenre({
+                ...response.data.genre.Genre,
+                matchingMovies: response.data.matchingMovies
+            });
+        } catch (error) {
+            console.log(error, 'could not GET User');
+        }
+    }
+
+    useEffect(() => {
+        getGenre();      
+    }, []);
+
     return (
         <Container xl={8} className="genre-view">
-            <h1>Hello</h1>
-            <Row className="genre-name text-center" lg={10}>
-                <Col>
-                    <h2 className="value">{genre.Name}</h2>
-                </Col>
-            </Row>
-            <Row className="genre-details">
-                <Col>
-                    <div className="genre-description">
-                        <h4 className="label">Description: </h4>
-                        <span className="value">{genre.Description}</span>
-                    </div>
-                    <div className="genre-movies">
-                        <h4 className="label">{genre.Name} Movies</h4>
-                        <Row>
-                            <Col className="card-columns">
-                                {genreMovies.map(movie => (
-                                    <MovieCard key={movie._id} movie={movie} />
-                                ))}
-                            </Col>
-                        </Row>
-                    </div>
-                    <Button className='movie-details' variant="secondary" onClick={() => navigate(-1)}>Back</Button>
-                </Col>
-            </Row>
+            {(!genre) ? 
+            <p>loading</p>
+            :
+            <div>
+                <Row className="genre-name text-center" lg={10}>
+                    <Col>
+                        <h2 className="value">{genre.Name}</h2>
+                    </Col>
+                </Row>
+                <Row className="genre-details">
+                    <Col>
+                        <div className="genre-description">
+                            <h4 className="label">Description: </h4>
+                            <span className="value">{genre.Description}</span>
+                        </div>
+                        <div className="genre-movies">
+                            <h4 className="label">{genre.Name} Movies</h4>
+                            <Row>
+                                <Col className="card-columns">
+                                    {genre.matchingMovies.map(movie => (
+                                        <MovieCard key={movie._id} movie={movie} />
+                                    ))}
+                                </Col>
+                            </Row>
+                        </div>
+                        <Button className='movie-details' variant="secondary" onClick={() => navigate(-1)}>Back</Button>
+                    </Col>
+                </Row>
+            </div>
+            } 
         </Container>
     );
 }
