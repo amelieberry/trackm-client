1d2c848d717a4587446ba45a8f09e1ff34675d3c
Amelie <aac.berry@gmail.com>
2022-11-16 17:44:19 +0100

    added user validation to update

--- src/components/profile-view/update-user.jsx
+++ src/components/profile-view/update-user.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import { Form, Button, Row, Card, CardGroup } from 'react-bootstrap';
 
-export function UpdateUser({ handleSubmit, user }) {
+export function UpdateUser({ handleUpdate, user }) {
     const [username, setUsername] = useState('');
     const [newPassword, setNewPassword] = useState('');
     const [email, setEmail] = useState('');
@@ -10,6 +10,49 @@ export function UpdateUser({ handleSubmit, user }) {
     const [usernameErr, setUsernameErr] = useState('');
     const [passwordErr, setPasswordErr] = useState('');
     const [emailErr, setEmailErr] = useState('');
+
+    // validate user input
+    const validate = () => {
+        let isReq = true;
+        if (username) {
+            if (username.length < 3) {
+                setUsernameErr('Username must contain at least 3 charaters');
+                isReq = false;
+            }
+        }
+
+        if (newPassword) {
+            if (newPassword.length < 6) {
+                setPasswordErr('Password must contain at least 6 characters');
+                isReq = false;
+            }
+        }
+
+        if (email) {
+            if (email.indexOf('@') === -1) {
+                setEmailErr('Email must be an email address')
+                isReq = false;
+            }
+        }
+        return isReq;
+    }
+
+    const handleSubmit = (e) => {
+        console.log(e);
+        e.preventDefault();
+        const isReq = validate();
+        if (isReq) {
+            let updateObject = {
+                Username: username ? username : user.Username,
+                Email: email ? email : user.Email,
+            };
+            if (newPassword) {
+                updateObject.Password = newPassword
+            };
+            console.log('submit', updateObject)
+            handleUpdate(updateObject);
+        }
+    }
 
     return (
         <div className="user-forms d-flex flex-column justify-content-center align-items-center">
@@ -19,7 +62,7 @@ export function UpdateUser({ handleSubmit, user }) {
             <CardGroup className="user-forms">
                 <Card bg="dark">
                     <Card.Body className="user-input">
-                        <Form className="profile-form" onSubmit={(e) => handleSubmit(e, username, newPassword, email, user)}>
+                        <Form className="profile-form" onSubmit={(e) => handleSubmit(e)}>
                             <Form.Group>
                                 <Form.Label>Username: </Form.Label>
                                 <Form.Control
@@ -28,6 +71,7 @@ export function UpdateUser({ handleSubmit, user }) {
                                     onChange={e => setUsername(e.target.value)}
                                     name="username"
                                 />
+                                {usernameErr && <p>{usernameErr}</p>}
                             </Form.Group>
 
                             <Form.Group>
@@ -38,6 +82,7 @@ export function UpdateUser({ handleSubmit, user }) {
                                     onChange={e => setNewPassword(e.target.value)}
                                     name="password"
                                 />
+                                {passwordErr && <p>{passwordErr}</p>}
                             </Form.Group>
 
                             <Form.Group>
@@ -48,6 +93,7 @@ export function UpdateUser({ handleSubmit, user }) {
                                     onChange={e => setEmail(e.target.value)}
                                     name="email"
                                 />
+                                {emailErr && <p>{emailErr}</p>}
                             </Form.Group>
 
                             <Button className="button-forms" variant="primary" type="submit" >Update Info</Button>
